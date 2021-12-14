e7bb5173b4e3ae1034a479392dafaf37505df1fe
Roman Kolivashko <rkolivashko@gmail.com>
2021-12-14 11:45:39 -0800

    created cleanup useEffect hook

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,12 @@
 import { Link } from 'react-router-dom';
 import styles from './Navbar.module.css';
 import Login from '../pages/login/Login';
+import { useLogout } from '../hooks/useLogout';
+
 
 export default function Navbar() {
+  const { logout} = useLogout();
+
   return (
     <div className={styles.navbar}>
       <ul>
@@ -10,6 +14,9 @@ export default function Navbar() {
         <li><Link to="/login" />Login
         </li>
         <li><Link to="/signup" />Signup</li>
+        <li>
+          <button className='btn' onClick={ logout }>Logout</button>
+        </li>
       </ul>
     </div>
   )
