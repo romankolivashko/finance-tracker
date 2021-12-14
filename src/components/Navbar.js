ce6271e5d4e3858bc77480ef380857b1c93bb9c5
Roman Kolivashko <rkolivashko@gmail.com>
2021-12-14 12:35:20 -0800

    added conditional showing of login and signup links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,21 +2,34 @@ import { Link } from 'react-router-dom';
 import styles from './Navbar.module.css';
 import Login from '../pages/login/Login';
 import { useLogout } from '../hooks/useLogout';
+import { useAuthContext } from '../hooks/useAuthContext';
 
 
 export default function Navbar() {
   const { logout} = useLogout();
+  const { user } = useAuthContext();
 
   return (
     <div className={styles.navbar}>
       <ul>
         <li className={styles.title}>myWallet</li>
-        <li><Link to="/login" />Login
-        </li>
-        <li><Link to="/signup" />Signup</li>
-        <li>
-          <button className='btn' onClick={ logout }>Logout</button>
-        </li>
+
+        {!user && (
+          <>
+            <li><Link to="/login">Login</Link>
+            </li>
+            <li><Link to="/signup">Signup</Link></li>
+          </>
+        )}
+
+        {user && (
+          <>
+            <li>Hello, { user.displayName }</li>
+              <li>
+                <button className='btn' onClick={ logout }>Logout</button>
+              </li>
+            </>
+        )}
       </ul>
     </div>
   )
