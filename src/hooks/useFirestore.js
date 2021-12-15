9eb546692c73dddd662a06c34c38c85c961556d2
Roman Kolivashko <rkolivashko@gmail.com>
2021-12-15 10:56:48 -0800

    Added dispatch logic and err handling to useFirestore hook

--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -10,7 +10,12 @@ let initialState = {
 
 const firestoreReducer = (state, action) => {
   switch (action.type) {
-
+    case 'IS_PENDING':
+      return { isPending: true, document: null, success: false, error: null } 
+    case 'ADDED_DOCUMENT':
+      return { isPending: false, document: action.payload, success: true, error: null }
+    case 'ERROR':
+      return { isPending: false, document: null, success: false, error: action.payload }
     default:
       return state
   }
@@ -23,10 +28,25 @@ export const useFirestore = (collection) => {
   //collection
   const ref = projectFirestore.collection(collection)
 
+  //only dispatch if not cancelled
+  const dispatchIfNotCancelled = (action) => {
+    if (!isCancelled) {
+      dispatch(action)
+    }
+  }
+
   //add document
   const addDocument = (doc) => {
+    dispatch({ type: 'IS_PENDING '})
 
+  try {
+    const addedDocument = await ref.add(doc)
+    dispatchIfNotCancelled({ type: 'ADDED_DOCUMENT', payload: addedDocument })
   }
+  catch(err){
+    dispatchIfNotCancelled({ type: 'ERROR', payload: err.message })
+  }
+}
 
   //delete document
   const deleteDocument = (id) => {
