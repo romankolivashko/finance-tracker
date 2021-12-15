55a7085267c939ff040e85584664f52c2c44e235
Roman Kolivashko <rkolivashko@gmail.com>
2021-12-14 16:04:50 -0800

    added route guards

--- src/App.js
+++ src/App.js
@@ -1,28 +1,37 @@
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/home/Home';
 import Login from './pages/login/Login';
 import Signup from './pages/signup/Signup';
+import { useAuthContext } from './hooks/useAuthContext'
 
 
 function App() {
+  const { authIsReady, user } = useAuthContext();
+
   return (
     <div className="App">
+      {authIsReady && (
       <BrowserRouter>
       <Navbar />
       <Switch>
         <Route exact path="/">
-            <Home />
+            {!user && <Redirect to="/login" />}
+            {user && <Home />}
         </Route>
         <Route path="/login">
+          {user && <Redirect to="/" />}
+          {!user && <Login />}
           <Login />
         </Route>
         <Route path="/signup">
+          {user &&<Redirect to="/" />}
+          {!user && <Signup />}
           <Signup />
         </Route>
       </Switch>
       </BrowserRouter>
-
+    )}
     </div>
   );
 }
