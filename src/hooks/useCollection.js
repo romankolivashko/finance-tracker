f0cd9f6e64b1ee0817d1ef5a8d5dd2977f49bbb9
Roman Kolivashko <rkolivashko@gmail.com>
2021-12-15 15:02:24 -0800

    Added firestore query to user-tag the expenses

--- src/hooks/useCollection.js
+++ src/hooks/useCollection.js
@@ -1,12 +1,23 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { projectFirestore } from "../firebase/config";
 
-export const useCollection = (collection) => {
+export const useCollection = (collection, _query) => {
   const [documents, setDocuments] = useState(null);
   const [error, setError] = useState(null);
 
+
+  //if we don't use a ref --> infinite loop in useEffect
+  // _query is an array and is "different" on every function call
+
+  const query = useRef(_query).current
+
   useEffect(() => {
     let ref = projectFirestore.collection(collection)
+
+    if (query) {
+      ref = ref.where( ...query)
+    }
+
     const unsubscribe = ref.onSnapshot((snapshot) => {
       let results = []
       snapshot.docs.forEach(doc => {
@@ -18,13 +29,13 @@ export const useCollection = (collection) => {
       setError(null)
     }, (error) => {
       console.log(error);
-      setError('could not fetch the ')
+      setError('could not fetch the data')
     })
 
     //unsubscribe on unmount
     return () => unsubscribe()
 
-  }, [collection]);
+  }, [collection, query]);
 
   return { documents, error }
 };
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -6,7 +6,12 @@ import TransactionList from './TransactionList'
 
 export default function Home() {
   const { user } = useAuthContext()
-  const { documents, error } = useCollection('transactions');
+  const { documents, error } = useCollection(
+    'transactions',
+    ['uid',
+    '==',
+    user.uid]
+    );
 
   return (
     <div className={styles.container}>
